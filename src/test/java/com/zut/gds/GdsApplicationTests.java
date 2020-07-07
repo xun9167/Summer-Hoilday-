807bb7dcda6c757816fb4b136f15edad995bbc19
huaxiaoyu <448898123@qq.com>
2020-07-07 17:32:05 +0800

    '初次创建项目'

---
+++ src/test/java/com/zut/gds/GdsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zut.gds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GdsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
