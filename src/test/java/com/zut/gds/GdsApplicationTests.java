90a90a1a6e13dbfe171d81050b7e284552965d79
huaxiaoyu <448898123@qq.com>
2020-07-09 17:03:06 +0800

    '完成公司列表的渲染'

--- src/test/java/com/zut/gds/GdsApplicationTests.java
+++ src/test/java/com/zut/gds/GdsApplicationTests.java
@@ -1,13 +1,31 @@
 package com.zut.gds;
 
+import com.zut.gds.entity.Companyfileinfo;
+import com.zut.gds.entity.Companyinfo;
+import com.zut.gds.entity.Teacherinfo;
+import com.zut.gds.service.CompanyfileinfoService;
+import com.zut.gds.service.CompanyinfoService;
+import com.zut.gds.service.TeacherinfoService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class GdsApplicationTests {
 
+    @Autowired
+    private CompanyinfoService companyinfoService;
+
+
+
     @Test
     void contextLoads() {
+        List<Companyinfo> list = companyinfoService.list();
+        for (Companyinfo companyinfo : list) {
+            System.out.println(companyinfo);
+        }
     }
 
 }
