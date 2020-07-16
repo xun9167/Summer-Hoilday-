802ff5a56e88f471e77bd14bf7127b76687ef3ad
github <916437341@qq.com>
2020-07-16 15:22:24 +0800

    admin  covered by xun

---
+++ src/main/java/com/zut/gds/controller/Company/StudentPwdController.java
@@ -0,0 +1,38 @@
+package com.zut.gds.controller.Company;
+
+import com.zut.gds.controller.Student.StudentBaseController;
+import com.zut.gds.entity.Studentinfo;
+import com.zut.gds.entity.Teacherinfo;
+import com.zut.gds.service.StudentinfoService;
+import com.zut.gds.service.TeacherinfoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpSession;
+//
+@Controller
+public class StudentPwdController extends StudentBaseController {
+    @Autowired
+    private TeacherinfoService teacherinfoService;
+    @Autowired
+    private StudentinfoService studentinfoService;
+    @GetMapping("/setpwd")
+    public String pwdPage(){
+        return "student/set_password";
+    }
+
+    @PostMapping("/changeadminpwd")
+    public String changepwd(@RequestParam("password")String password,
+                            HttpSession session
+    ){
+
+        Integer studentLoginId = (Integer)session.getAttribute("loginid");
+        Studentinfo tbyId=studentinfoService.getById(studentLoginId);
+        tbyId.setStudentPhone(password);
+        studentinfoService.saveOrUpdate(tbyId);
+        return "redirect:/student/setpwd";
+    }
+}
